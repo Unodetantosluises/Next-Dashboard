2cdc00adead9aadcc2dd97dcb64b911f77d7d374
Luis <luisantoniodiaz97@gmail.com>
2024-01-22 11:54:44 -0600

    Monserat

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
 import { Inter, Montserrat } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin']});
+// Fuente Montserrat
 export const mont = Montserrat({ weight: ['400', '700'], subsets: ['latin']})
